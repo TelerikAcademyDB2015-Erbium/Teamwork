eace4e77ffed685e28de3a30788ac039a6bd1d6a
GeorgiNik <georgi.nik95@gmail.com>
2015-10-18 21:41:08 +0300

    UniqueFileNameGenerator for pdf reports

---
+++ AutopartsSystem/AutopartsSystem.Core/Pdf/UniqueFileNameGenerator.cs
@@ -0,0 +1,24 @@
+﻿namespace AutopartsSystem.Core.Pdf
+{
+    using System;
+
+    public static class UniqueFileNameGenerator
+    {
+        public static string AddUniqueFilenameSuffix(string fileName)
+        {
+            DateTime currentDate = DateTime.Now;
+            var fileNameSuffix =
+                string.Format(
+                "-{0}.{1}.{2}-{3}.{4}.{5}.pdf",
+                    currentDate.Day,
+                    currentDate.Month,
+                    currentDate.Year,
+                    currentDate.Hour,
+                    currentDate.Minute,
+                    currentDate.Second);
+
+            fileName = fileName + fileNameSuffix;
+            return fileName;
+        }
+    }
+}
