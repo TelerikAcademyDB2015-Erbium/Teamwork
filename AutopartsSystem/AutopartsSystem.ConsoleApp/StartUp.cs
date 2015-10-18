08471bb75b79bd11f6e880b834dfd5666d9a630a
Pavel <pawel.angelow@gmail.com>
2015-10-18 15:56:16 +0300

    Added Entity Framework and linked inside the solution

--- AutopartsSystem/AutopartsSystem.ConsoleApp/StartUp.cs
+++ AutopartsSystem/AutopartsSystem.ConsoleApp/StartUp.cs
@@ -1,9 +1,15 @@
 ﻿namespace AutopartsSystem.ConsoleApp
 {
+    using Data;
+    using Models;
+
     class Startup
     {
         static void Main()
         {
+            var db = new AutopartsDbContext();
+
+            db.Manufacturers.Add(new Manufacturer() { Name = "Bosch"});
         }
     }
 }
---
+++ AutopartsSystem/AutopartsSystem.Data/AutopartsDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AutopartsSystem.Models;
+
+namespace AutopartsSystem.Data
+{
+    using System.Data.Entity;
+
+    public class AutopartsDbContext : DbContext
+    {
+        public AutopartsDbContext() : base("CarPartsSystem")
+        {   
+        }
+
+        public virtual IDbSet<AutoPart> AutoParts { get; set; }
+
+        public virtual IDbSet<CarBrand> CarBrands { get; set; }
+        
+        public virtual IDbSet<CarModel> CarModels { get; set; }
+        
+        public virtual IDbSet<Compatibility> Compatibilities { get; set; }
+        
+        public virtual IDbSet<Manufacturer> Manufacturers { get; set; }
+        
+        public virtual IDbSet<PartType> PartTypes { get; set; } 
+    }
+}
