49287bd17f1992387fe4c9f3106c6fd0dde614d4
GeorgiNik <georgi.nik95@gmail.com>
2015-10-19 11:23:15 +0300

    Added some changes in AutoPart.cs

--- AutopartsSystem/AutopartsSystem.Models/AutoPart.cs
+++ AutopartsSystem/AutopartsSystem.Models/AutoPart.cs
@@ -12,9 +12,13 @@
 
         public Compatibility Compatibility { get; set; }
 
+        public int ManufacturerId { get; set; }
+
         public Manufacturer Manufacturer { get; set; }
 
         public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
 
         public string Description { get; set; }
 
