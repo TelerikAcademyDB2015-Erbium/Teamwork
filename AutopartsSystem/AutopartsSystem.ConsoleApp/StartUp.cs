c9b1894cf675a874989f0ee1ddd5db936c1c0729
Pavel <pawel.angelow@gmail.com>
2015-10-19 12:04:42 +0300

    Added XlsReader

--- AutopartsSystem/AutopartsSystem.ConsoleApp/StartUp.cs
+++ AutopartsSystem/AutopartsSystem.ConsoleApp/StartUp.cs
@@ -1,65 +1,36 @@
 ﻿namespace AutopartsSystem.ConsoleApp
 {
-    using System;
     using System.Collections.Generic;
-    using System.Data;
-    using System.Data.OleDb;
-    using System.Text;
-    using AutopartsSystem.Data;
-    using AutopartsSystem.Data.Migrations;
-    using AutopartsSystem.Models;
+    using System.Data.Entity;
     using Core.Common;
-    using Ionic.Zip;
+    using Core.Parsers.Excel;
+    using Data;
+    using Data.Migrations;
 
     public class Startup
     {
         public static void Main()
         {
-            ////Database.SetInitializer(new MigrateDatabaseToLatestVersion<AutopartsDbContext,Configuration>());
-            ////var db = new AutopartsDbContext();
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AutopartsDbContext, Configuration>());
+            var db = new AutopartsDbContext();
 
-            ////db.Manufacturers.Add(new Manufacturer() { Name = "Bosch" });
-            ////db.SaveChanges();
-            ////Console.WriteLine(db.Manufacturers.Count());
+            // db.Manufacturers.Add(new Manufacturer() { Name = "Bosch" });
+            // db.SaveChanges();
+            // Console.WriteLine(db.Manufacturers.Count());
 
-            //// ZipFile zipFile = new ZipFile(Constants.PathToFiles + "/Files.zip");
-            //// zipFile.ExtractAll(Constants.PathToFiles + "/Extracted/");
-            //// Console.WriteLine(Constants.PathToFiles);
+            // ZipFile zipFile = new ZipFile(Constants.PathToFiles + "/Files.zip");
+            // zipFile.ExtractAll(Constants.PathToFiles + "/Extracted/");
+            // Console.WriteLine(Constants.PathToFiles);
+            var theReader = new XlsReader();
 
-            Dictionary<string, string> props = new Dictionary<string, string>();
-            props["Provider"] = "Microsoft.Jet.OLEDB.4.0";
-            props["Data Source"] = Constants.PathToFiles + "/1.xls";
-            props["Extended Properties"] = "Excel 8.0";
+            var pathToTheXLS = Constants.PathToFiles + "/1.xls";
             var columnNames = new List<string>()
             {
                 "Name", "Description", "Price", "Compatibility", "Manufacturer", "Type"
             };
+            var sheetName = "Parts";
 
-            StringBuilder sb = new StringBuilder();
-            foreach (KeyValuePair<string, string> prop in props)
-            {
-                sb.Append(prop.Key);
-                sb.Append('=');
-                sb.Append(prop.Value);
-                sb.Append(';');
-            }
-
-            string properties = sb.ToString();
-
-            using (OleDbConnection conn = new OleDbConnection(properties))
-            {
-                conn.Open();
-                DataSet ds = new DataSet();
-                string columns = string.Join(",", columnNames.ToArray());
-                using (OleDbDataAdapter da = new OleDbDataAdapter(
-                    "SELECT " + columns + " FROM [" + "People" + "$]", conn))
-                {
-                    DataTable dt = new DataTable("temp");
-                    da.Fill(dt);
-                    var mhm = dt.Rows[0];
-                    ds.Tables.Add(dt);
-                }
-            }
+            var contentToAdd = theReader.ProvideContent(pathToTheXLS, sheetName, columnNames);
         }
     }
 }
---
+++ AutopartsSystem/AutopartsSystem.Core/Parsers/Excel/XlsReader.cs
@@ -0,0 +1,74 @@
+﻿namespace AutopartsSystem.Core.Parsers.Excel
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.OleDb;
+    using System.Linq;
+    using System.Text;
+    using Common;
+
+    public class XlsReader
+    {
+        public IEnumerable<string> ProvideContent(string filePath, string sheetName, IEnumerable<string> columnOrder)
+        {
+            var contentToReturn = new List<string>();
+            var currentRow = new StringBuilder();
+
+            Dictionary<string, string> props = new Dictionary<string, string>();
+            props["Provider"] = "Microsoft.Jet.OLEDB.4.0";
+            props["Data Source"] = filePath;
+            props["Extended Properties"] = "Excel 8.0";
+
+            StringBuilder sb = new StringBuilder();
+            foreach (KeyValuePair<string, string> prop in props)
+            {
+                sb.Append(prop.Key);
+                sb.Append('=');
+                sb.Append(prop.Value);
+                sb.Append(';');
+            }
+
+            string properties = sb.ToString();
+
+            using (OleDbConnection conn = new OleDbConnection(properties))
+            {
+                conn.Open();
+                DataSet ds = new DataSet();
+                string columns = string.Join(",", columnOrder.ToArray());
+                using (OleDbDataAdapter da = new OleDbDataAdapter(
+                    "SELECT " + columns + " FROM [" + sheetName + "$]", conn))
+                {
+                    DataTable dt = new DataTable("temp");
+
+                    da.Fill(dt);
+                    var allRows = dt.Rows;
+                    for (int i = 0; i < allRows.Count; i++)
+                    {
+                        var currentRowElements = allRows[i].ItemArray;
+                        if (!string.IsNullOrEmpty(currentRowElements[0].ToString()))
+                        {
+                            currentRow.Append(currentRowElements[0].ToString());
+                            currentRow.Append(Constants.DividerForExcelRead);
+                            currentRow.Append(currentRowElements[1].ToString());
+                            currentRow.Append(Constants.DividerForExcelRead);
+                            currentRow.Append(currentRowElements[2].ToString());
+                            currentRow.Append(Constants.DividerForExcelRead);
+                            currentRow.Append(currentRowElements[3].ToString());
+                            currentRow.Append(Constants.DividerForExcelRead);
+                            currentRow.Append(currentRowElements[4].ToString());
+                            currentRow.Append(Constants.DividerForExcelRead);
+                            currentRow.Append(currentRowElements[5].ToString());
+
+                            contentToReturn.Add(currentRow.ToString());
+                            currentRow.Clear();
+                        }
+                    }
+
+                    ds.Tables.Add(dt);
+                }
+            }
+
+            return contentToReturn;
+        }
+    }
+}
