0e4c8fa2de5364cc61fcfaa40108658a31e73d08
GeorgiNik <georgi.nik95@gmail.com>
2015-10-19 13:18:59 +0300

    StartUp.cs added test for pdf report

--- AutopartsSystem/AutopartsSystem.ConsoleApp/StartUp.cs
+++ AutopartsSystem/AutopartsSystem.ConsoleApp/StartUp.cs
@@ -1,6 +1,10 @@
 ﻿namespace AutopartsSystem.ConsoleApp
 {
     using System.Data.Entity;
+
+    using AutopartsSystem.Core.Pdf;
+    using AutopartsSystem.Models;
+
     using Core.Parsers.Excel;
     using Data;
     using Data.Migrations;
@@ -19,6 +23,13 @@
             // ZipFile zipFile = new ZipFile(Constants.PathToFiles + "/Files.zip");
             // zipFile.ExtractAll(Constants.PathToFiles + "/Extracted/");
             // Console.WriteLine(Constants.PathToFiles);
+
+            //var autopart = new AutoPart { Description = "bla bla bla", Manufacturer = new Manufacturer { Name = "PeshoMotors" }, Quantity = 30, Price = 500, Name = "Driving wheel" };
+            //db.AutoParts.Add(autopart);
+            //db.SaveChanges();
+            //var report = new PdfReport();
+            //report.GenerateAutopartsReports(@"../../../Reports/PDF/", "joro", db);
+
             var parser = new XlsToAutoPart(db);
             parser.InsertDataIntoDB();
         }
